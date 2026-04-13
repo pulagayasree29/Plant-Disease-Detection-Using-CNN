40922fdaa6e62af86ce84f8a4b9dcd9b8a47730b
PULA GAYASREE <103738648+pulagayasree29@users.noreply.github.com>
2026-04-14 00:40:59 +0530

    Delete app.py

--- app.py
+++
@@ -1,7 +0,0 @@
-import streamlit as st
-
-st.title("🌿 Leaf Disease Detection System")
-st.write("This is a test to ensure basic deployment works.")
-
-if st.button("Test Button"):
-    st.success("Button clicked! App is working.")
